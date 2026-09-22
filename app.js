d9ca0e8f752ef0194483e3b0b860d4037199914c
alefdesouza7-png <alefdesouza7@gmail.com>
2026-09-22 14:54:30 -0300

    Corrige fechamento da função de estatísticas

--- app.js
+++ app.js
@@ -1208,6 +1208,7 @@ function normalizarEstatisticas(dados) {
         )
     };
   });
+}
   /* =========================================================
    CARREGAMENTO COMPLETO DA PARTIDA
 ========================================================= */
